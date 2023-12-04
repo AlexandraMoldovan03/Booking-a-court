c38a2f18f7dedf200ed07503dc4bdb5e19ebec16
AlexandraMoldovan03 <147625508+AlexandraMoldovan03@users.noreply.github.com>
2023-12-04 04:34:34 +0200

    Add files via upload

---
+++ new/navbar.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light px-lg-3 py-lg-2 shadow-sm sticky-top">
+  <div class="container-fluid">
+
+    <h1><a class="navbar-brand" href="index.php">Tenis Club Sun</a></h1>
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="welcome.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link me-2" href="#">Courts</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link me-2" href="#">Contact us</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link me-2" href="#">Book a court</a>
+        </li>
+</ul>
+    <form class="d-flex" >
+        <a class="btn btn-outline-success mx-2" type="submit" href="signup.php">Signup</a>
+        <a class="btn btn-outline-primary mx-2" type="submit" href="login.php">Login</a>
+        <a class="btn btn-outline-danger mx-2" type="submit" href="logout.php">Logout</a>
+    </form>
+    
+
+  </div>
+</nav>
